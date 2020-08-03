de9bd9ed12db969c7f35d68ea496f3009f92a4ce
vivian dang <vivian.t.dang10@gmail.com>
2020-08-03 16:26:05 -0700

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Vivian Dang on 8/3/20.
+//  Copyright © 2020 Vivian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
